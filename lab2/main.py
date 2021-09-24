03cb34d1dbe21ce50889932a4944f4d2049f73d0
Oscar Pfuturi <49136821+OscarDPfuturi@users.noreply.github.com>
2021-09-24 18:37:04 -0500

    Update main.py

--- lab2/main.py
+++ lab2/main.py
@@ -5,38 +5,28 @@ LETRAS = ("ABCDEFGHIJKLMNÑOPQRSTUVWXYZ")
 def main():
     file = open('plain.txt','r', encoding='utf-8')
     mensaje=str(file.read())
+    #mensaje=mensaje[2:] # 2 caracteres inesperados
     mensaje=met.removeTildes(mensaje)
     mensaje=met.toUpper(mensaje)
     mensaje=met.removeSignos(mensaje)
     myKey="POSITIVO"
-    traducido=Vignere_cipher(mensaje,myKey)
+    traducido=Vignere_cipher(mensaje,myKey,True)
     print(traducido)
-    traducido=Vignere_decipher(traducido,myKey)
+    traducido=Vignere_cipher(traducido,myKey,False)
     print(traducido)
 
-def Vignere_cipher(message,key):
+def Vignere_cipher(message,key,action):
     i=0
     mssg_cipher=[]
+    d=1
+    if action==False:
+        d=-1
     for caracter in message:
         k=key[i]
         num=LETRAS.find(caracter)
-        num2=LETRAS.find(k)
+        num2=LETRAS.find(k)*(d)
         #print(num,num2)
         newnum=(num+num2)%len(LETRAS)
-        mssg_cipher.append(LETRAS[newnum])
-        if i==len(key)-1: i=0
-        else : i+=1
-    return ''.join(mssg_cipher)
-
-def Vignere_decipher(message,key):
-    i=0
-    mssg_cipher=[]
-    for caracter in message:
-        k=key[i]
-        num=LETRAS.find(caracter)
-        num2=LETRAS.find(k)
-        #print(num,num2)
-        newnum=(num-num2)%len(LETRAS)
         mssg_cipher.append(LETRAS[newnum])
         if i==len(key)-1: i=0
         else : i+=1
