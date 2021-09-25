ee5f37f5285b1de4dcf1b7a01fee8be987800802
Oscar Pfuturi <49136821+OscarDPfuturi@users.noreply.github.com>
2021-09-24 19:04:00 -0500

    Update main.py

--- lab2/main.py
+++ lab2/main.py
@@ -3,17 +3,15 @@ import methods as met
 LETRAS = ("ABCDEFGHIJKLMNÑOPQRSTUVWXYZ")
 
 def main():
-    file = open('plain.txt','r', encoding='utf-8')
+    file = open('texto.txt','r', encoding='utf-8')
     mensaje=str(file.read())
-    #mensaje=mensaje[2:] # 2 caracteres inesperados
     mensaje=met.removeTildes(mensaje)
     mensaje=met.toUpper(mensaje)
     mensaje=met.removeSignos(mensaje)
     myKey="POSITIVO"
     traducido=Vignere_cipher(mensaje,myKey,True)
-    print(traducido)
-    traducido=Vignere_cipher(traducido,myKey,False)
-    print(traducido)
+    #print(traducido)
+    met.frecuencias(traducido)
 
 def Vignere_cipher(message,key,action):
     i=0
